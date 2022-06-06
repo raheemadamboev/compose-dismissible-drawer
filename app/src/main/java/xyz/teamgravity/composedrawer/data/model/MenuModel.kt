4efb2c6ba7b2d2fe8394106455922072f3027c25
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-06 13:19:23 +0500

    add menu model

---
+++ app/src/main/java/xyz/teamgravity/composedrawer/data/model/MenuModel.kt
@@ -0,0 +1,10 @@
+package xyz.teamgravity.composedrawer.data.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class MenuModel(
+    val id: String,
+    val title: String,
+    val contentDescription: String,
+    val icon: ImageVector,
+)
